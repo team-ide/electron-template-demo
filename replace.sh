56e1c0a64da63e490a17332b38f52877949fd291
朱亮 <teamide@163.com>
2023-01-11 17:23:35 +0800

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -5,11 +5,22 @@ cd `dirname $0`
 
 echo `pwd`
 
+templateDir=./electron-template
+# 复制 应用信息 package.json 到 release/app
 echo 'cp package.json'
-cp -rf package.json electron-template/release/app/package.json
-echo 'cp config.ts'
-cp -rf config.ts electron-template/src/main/config.ts
+cp -rf package.json $templateDir/release/app/package.json
 
+echo 'app package.json info'
+cat $templateDir/release/app/package.json
+
+# 复制 应用配置 config.ts 到 src/main
+echo 'cp config.ts'
+cp -rf config.ts $templateDir/src/main/config.ts
+
+echo 'config.ts info'
+cat $templateDir/src/main/config.ts
+
+# 设置 应用 变量
 productName='ElectronTemplateDemo'
 publisherName='ZhuLiang'
 publishProvider='github'
@@ -23,11 +34,18 @@ echo 'set publishOwner='$publishOwner
 echo 'set publishRepo='$publishRepo
 
 # 设置包相关信息
-echo 'replace productName'
-sed -i 's/<productName>/'$productName'/g' ./electron-template/package.json
-echo 'replace publisherName'
-sed -i 's/<publisherName>/'$publisherName'/g' ./electron-template/package.json
-echo 'replace publish'
-sed -i 's/"<publish>"/{"provider": "'$publishProvider'","owner": "'$publishOwner'","repo": "'$publishRepo'"}/g' ./electron-template/package.json
 
-cat ./electron-template/package.json
+# 设置 项目名称
+echo 'replace productName'
+sed -i 's/<productName>/'$productName'/g' $templateDir/package.json
+
+# 设置 项目 发布者
+echo 'replace publisherName'
+sed -i 's/<publisherName>/'$publisherName'/g' $templateDir/package.json
+
+# 设置 项目 发布信息
+echo 'replace publish'
+sed -i 's/"<publish>"/{"provider": "'$publishProvider'","owner": "'$publishOwner'","repo": "'$publishRepo'"}/g' $templateDir/package.json
+
+echo 'package.json info'
+cat $templateDir/package.json
